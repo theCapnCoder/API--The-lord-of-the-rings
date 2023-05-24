9c5189acde96ef7d8bd55e1999aa2e17fdc209e1
PavelM <myagkiypavel@gmail.com>
2023-05-24 10:35:44 +0300

    feature: add main routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import { Chracters } from "../pages/Home/Characters";
+import { Quotes } from "../pages/Home/Quotes";
+import { Chapters } from "../pages/Home/Chapters";
+import { books } from "./BooksRoutes";
+import { movies } from "./MoviesRoutes";
+import { characters } from "./CharactersRoutes";
+import { quotes } from "./QuotesRoutes";
+import { chapters } from "./ChaptersRoutes";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    children: [...movies, ...books, ...characters, ...quotes, ...chapters],
   },
-  {
-    path: "/news",
-    element: <div>News</div>,
-  }
 ]);
 
 export default routes;
