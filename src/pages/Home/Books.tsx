daf6ebe6d7230d4e28a88064e4fdfc94a38fcb0f
PavelM <myagkiypavel@gmail.com>
2023-05-24 15:47:07 +0300

    chore: show all books

--- src/pages/Home/Books.tsx
+++ src/pages/Home/Books.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../redux/store/type";
+import { Book } from "../../redux/reducers/booksReducer/booksSlice";
+import { getAllBooks } from "../../redux/reducers/booksReducer/actionCreators/getAllBooks";
 
 export const Books = () => {
+  const books = useSelector((state: RootState) => state.books);
+  const dispatch = useDispatch<AppDispatch>();
+  console.log(books)
+
+  useEffect(() => {
+    dispatch(getAllBooks());
+  }, [dispatch]);
+
   return (
-    <div>Books</div>
-  )
-}
+    <div>
+      <h1>Books</h1>
+      {books.books.map((book: Book) => (
+        <div key={book._id}>
+          <h2>{book._id}</h2>
+          <h3>{book.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
