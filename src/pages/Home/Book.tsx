2af7ac06248fe027b68fc140b26ce4631d449286
PavelM <myagkiypavel@gmail.com>
2023-05-25 14:32:42 +0300

    feature: add Book

--- src/pages/Home/Book.tsx
+++ src/pages/Home/Book.tsx
@@ -9,40 +9,59 @@ import {
 import { useNavigate, useParams } from "react-router";
 import { Book as TBook } from "../../redux/reducers/booksReducer/booksSlice";
 import instance from "../../api/instance";
+import ChaptersModal from "./ChaptersModal";
 
 export const Book = () => {
   const [book, setBook] = useState<TBook | null>(null);
+  const [openModal, setOpenModal] = useState(false);
   const { id } = useParams();
   const navigate = useNavigate();
+
+  const handleOpenModal = () => {
+    setOpenModal((prev) => !prev);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal((prev) => !prev);
+  };
 
   const goBack = () => {
     navigate(-1);
   };
 
   useEffect(() => {
-    instance
-      .get(`book/${id}`)
-      .then((res) => setBook(res.data.docs[0]));
+    instance.get(`book/${id}`).then((res) => setBook(res.data.docs[0]));
   }, [id]);
 
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          ID: {book?._id}
-        </Typography>
-        <Typography
-          sx={{ mb: 1.5, fontSize: 28, fontWeight: "bold" }}
-          color="text.secondary"
-        >
-          {book?.name}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button onClick={goBack} size="small">
-          Back
-        </Button>
-      </CardActions>
-    </Card>
+    <>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography
+            sx={{ mb: 1.5, fontSize: 28, fontWeight: "bold" }}
+            color="text.secondary"
+          >
+            {book?.name}
+          </Typography>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            ID: {book?._id}
+          </Typography>
+        </CardContent>
+
+        <CardActions>
+          <Button onClick={goBack} size="small" variant="outlined">
+            Back
+          </Button>
+          {book && (
+            <ChaptersModal
+              id={book._id}
+              openModal={openModal}
+              handleOpen={handleOpenModal}
+              handleClose={handleCloseModal}
+            />
+          )}
+        </CardActions>
+      </Card>
+    </>
   );
 };
