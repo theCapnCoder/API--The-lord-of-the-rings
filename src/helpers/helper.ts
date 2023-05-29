2cca81c4ec07ca05b8c1e9ca6d450d4594f7dba0
PavelM <myagkiypavel@gmail.com>
2023-05-29 17:05:25 +0300

    feature: helper function

---
+++ src/helpers/helper.ts
@@ -0,0 +1,8 @@
+export function getChunkOfTenElements(objects: any[], number: string | undefined) {
+  const num = number ? Number(number) : 1;
+  const startIndex = (num - 1) * 10;
+  const endIndex = startIndex + 10;
+  const chunk = objects.slice(startIndex, endIndex);
+  return chunk;
+}
+
