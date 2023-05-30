31ef1f7d8c78f752ba14e11a11e48e56fbfcd6ea
PavelM <myagkiypavel@gmail.com>
2023-05-30 19:19:39 +0300

    chore: PaginationRadioButton

--- src/pages/Quote/PaginationRadioButton.tsx
+++ src/pages/Quote/PaginationRadioButton.tsx
@@ -11,12 +11,6 @@ type Props = {
 };
 
 export default function PaginationRadioButtons({ onChange }: Props) {
-  const [value, setValue] = useState(Pagination.limit);
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value as Pagination);
-  };
-
   return (
     <FormControl>
       <FormLabel id="demo-radio-buttons-group-label">Type Pagination</FormLabel>
@@ -47,35 +41,3 @@ export default function PaginationRadioButtons({ onChange }: Props) {
     </FormControl>
   );
 }
-
-// import * as React from 'react';
-// import FormControl from '@mui/material/FormControl';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import FormLabel from '@mui/material/FormLabel';
-// import Radio from '@mui/material/Radio';
-// import RadioGroup from '@mui/material/RadioGroup';
-
-// export enum Pagination {
-//   limit = 'limit',
-//   page = 'page',
-//   offset = 'offset'
-// }
-
-// export default function RadioButtonsGroup() {
-//   const [value, setValue] = React.useState(Pagination.limit);
-
-//   const handleChange = (event) => {
-//     setValue(event.target.value);
-//   };
-
-//   return (
-//     <FormControl component="fieldset">
-//       <FormLabel component="legend">Pagination</FormLabel>
-//       <RadioGroup aria-label="pagination" name="pagination" value={value} onChange={handleChange}>
-//         <FormControlLabel value={Pagination.limit} control={<Radio />} label="Limit" />
-//         <FormControlLabel value={Pagination.page} control={<Radio />} label="Page" />
-//         <FormControlLabel value={Pagination.offset} control={<Radio />} label="Offset" />
-//       </RadioGroup>
-//     </FormControl>
-//   );
-// }
