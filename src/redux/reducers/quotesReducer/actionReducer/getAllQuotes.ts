e72655cd0319b3213acd141ec59b8e6902b1853c
PavelM <myagkiypavel@gmail.com>
2023-05-30 19:17:08 +0300

    feature: getAllQuotes

---
+++ src/redux/reducers/quotesReducer/actionReducer/getAllQuotes.ts
@@ -0,0 +1,18 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import instance from '../../../../api/instance'
+
+// export enum Pagination {
+//   limit = 'limit',
+//   page = 'page',
+//   offset = 'offset'
+// }
+
+export const getAllQuotes = createAsyncThunk(
+  'quotes/getAllQuotes',
+  async (typePagination: Pagination) => {
+    const response = await instance.get(`/quote?${typePagination}=20`)
+    const data = response.data
+
+    return data;
+  }
+)
