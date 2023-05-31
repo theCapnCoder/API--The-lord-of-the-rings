e5efcfc343afb58bfdaf4a06ea137bb47626730f
PavelM <myagkiypavel@gmail.com>
2023-05-31 19:18:39 +0300

    chore: QuoteList

--- src/pages/Quote/QuoteList.tsx
+++ src/pages/Quote/QuoteList.tsx
@@ -2,37 +2,37 @@ import { useEffect, useState, ChangeEvent } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../redux/store/type";
 import {
-  Pagination,
+  Pagination as TPagination,
   getAllQuotes,
 } from "../../redux/reducers/quotesReducer/actionReducer/getAllQuotes";
 import PaginationRadioButtons from "./PaginationRadioButton";
+import Pagination from "./Pagination";
 
 export const QuoteList = () => {
   const quotes = useSelector((state: RootState) => state.quotes);
   const dispatch = useDispatch<AppDispatch>();
 
-  const [typePagination, setTypePagination] = useState<Pagination>(Pagination.limit);
+  const [typePagination, setTypePagination] = useState<TPagination>(
+    TPagination.limit
+  );
 
   console.log({ typePagination });
   console.log(quotes);
 
   const handleTypePagination = (value: ChangeEvent<HTMLInputElement>) => {
-    setTypePagination(value.target.value as Pagination);
+    setTypePagination(value.target.value as TPagination);
   };
 
   console.log({ typePagination });
 
-  // const enumValue: Pagination = stringValue as Pagination;
-  // console.log(enumValue); // Output: "offset"
-
   useEffect(() => {
-
     dispatch(getAllQuotes(typePagination));
   }, [dispatch, typePagination]);
 
   return (
     <div>
       <h3>QuoteList</h3>
+      <Pagination count={quotes.total} />
       <PaginationRadioButtons onChange={handleTypePagination} />
       <div></div>
     </div>
