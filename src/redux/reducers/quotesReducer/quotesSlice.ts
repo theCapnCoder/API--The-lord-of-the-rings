2984bccc48e807948807a42fd8cfc06f4e6db027
PavelM <myagkiypavel@gmail.com>
2023-05-31 19:17:29 +0300

    chore: quoteSlice

--- src/redux/reducers/quotesReducer/quotesSlice.ts
+++ src/redux/reducers/quotesReducer/quotesSlice.ts
@@ -1,16 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { getAllQuotes } from './actionReducer/getAllQuotes';
 
+type State = {
+  quotes: any[];
+  total: number;
+}
+
+const initialState: State = {
+  quotes: [],
+  total: 0
+}
+
 const quotesSlice = createSlice({
   name: 'quotes',
-  initialState: {
-    quotes: []
-  },
+  initialState,
   reducers: {
   },
   extraReducers: (builder) => {
     builder.addCase(getAllQuotes.fulfilled, (state, action) => {
       state.quotes = action.payload.docs
+      state.total = action.payload.total
     })
   }
 
